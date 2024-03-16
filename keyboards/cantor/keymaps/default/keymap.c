6164757069e232000b49ae9dad3d90faf8673e00
Richard Marklund <richard.marklund@wirelesscar.com>
2024-03-16 18:38:59 +0100

    fix: reformat default cantor layout

--- keyboards/cantor/keymaps/default/keymap.c
+++ keyboards/cantor/keymaps/default/keymap.c
@@ -2,27 +2,74 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include QMK_KEYBOARD_H
+#include "keymap_swedish.h"
+
+enum layers {
+    _QWERTY = 0,
+    _NUM,
+    _SIGN,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
+      * │ESC│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ Å │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Ctl│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
+      * │Sft│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ O │ Ä │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
+      * │Ctl│ Z │ X │ C │ V │ B │       │ N │ M │ ,;│ .:│'* │ +?│
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
-      *               │GUI├───┐           ┌───┤Alt│
-      *               └───┤   ├───┐   ┌───┤   ├───┘
-      *                   └───┤Bsp│   │Ent├───┘
+      *               │GUI├───┐           ┌───┤Opt│
+      *               └───┤Tab├───┐   ┌───┤Bsp├───┘
+      *                   └───┤Ent│   │Spc├───┘
+      *                       │Num├───┤Sgn│
+      *                       └───┘   └───┘
+      */                        
+    [_QWERTY] = LAYOUT_split_3x6_3(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,            KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC ,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,            KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,            KC_M,     KC_COMM, KC_DOT,  KC_BSLS, KC_MINS,
+                                            KC_TAB,  KC_LGUI, LT(_NUM,KC_ENT),        LT(_SIGN,KC_SPC),KC_BSPC, KC_LEFT_ALT 
+    ),
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │ * │ ! │ @ │ { │ } │ = │       │   │ < │ > │   │   │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │   │ " │ ( │ ) │ - │       │ | │ & │ ; │   │   │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │ % │ # │ [ │ ] │ _ │       │   │   │   │   │   │   │
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │GUI├───┐           ┌───┤ = │
+      *               └───┤Bsp├───┐   ┌───┤TAB├───┘
+      *                   └───┤Ent│   │   ├───┘
       *                       └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
+    [_SIGN] = LAYOUT_split_3x6_3(
+        SE_ASTR, KC_EXLM, SE_AT,   LALT(S(SE_8)), LALT(S(SE_9)), S(SE_0),                     KC_NO,  KC_GRV,     KC_TILD, KC_NO,    KC_NO, KC_NO,
+        KC_NO,   KC_NO,   KC_AT,   KC_ASTR,       KC_LPRN,       SE_MINS,                     KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_NO, KC_NO,
+        KC_NO,   KC_PERC, KC_HASH, LALT(SE_8),    LALT(SE_9),    SE_UNDS,                     KC_NO,  LALT(SE_7), SE_AMPR,  KC_SCLN,  KC_NO, KC_NO,
+                                                  KC_LGUI,       KC_BSPC, KC_ENT,     KC_NO,  KC_TAB, S(SE_0)
+    ),
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │   │ * │ 7 │ 8 │ 9 │ - │       │   │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │ / │ 4 │ 5 │ 6 │ + │       │   │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │   │ 0 │ 1 │ 2 │ 3 │TAB│       │   │   │   │   │   │   │
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │Bsp├───┐           ┌───┤   │
+      *               └───┤Ent├───┐   ┌───┤   ├───┘
+      *                   └───┤   │   │   ├───┘
+      *                       └───┘   └───┘
+      */
+    [_NUM] = LAYOUT_split_3x6_3(
+        KC_NO, KC_PIPE, KC_7,    KC_8,    KC_9,    SE_MINS,                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, S(SE_7), KC_4,    KC_5,    KC_6,    KC_MINS,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_0,    KC_1,    KC_2,    KC_3,    KC_TAB,                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                            KC_BSPC, KC_ENT, KC_NO ,      KC_NO, KC_NO, KC_NO 
     )
 };
-
--- keyboards/splitkb/kyria/keymaps/richardmarklund/keymap.c
+++ keyboards/splitkb/kyria/keymaps/richardmarklund/keymap.c
@@ -23,9 +23,14 @@ enum layers {
     _ADJUST,
 };
 
+enum custom_keycodes {
+    MACRO1 = SAFE_RANGE,
+};
+
 #define QWERTY DF(_QWERTY)
 #define LOWER DF(_LOWER)
 #define RAISE DF(_RAISE)
+#define RGBLIGHT_SPLIT
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -36,34 +41,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
      * |LShift  |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  |   Ö  |  Ä     |
      * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-     * |Ctrl    |   Z  |   X  |   C  |   V  |   B  |   (  |   ;  |  |   {  |  =   |   N  |   M  | ,  ; | . :  | +  ? |  - _   |
+     * |Ctrl    |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  ; | . :  |  '*  |  +?    |
      * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
-     *                        | *    |ALT+  | GUI  | BackS| Del  |  | Enter| Space| Tab  | Bksp | AltGr|
-     *                        |      | TAB  |      |      |Lower |  | Raise|      |      |      |      |
+     *                        |Option|  TAB | GUI  | BackS| Del  |  | Enter| Space|      |      |      |
+     *                        |      |      |      |      |Lower |  | Raise|      |      |      |      |
      *                        `----------------------------------'  `----------------------------------'
      */
     [_QWERTY] = LAYOUT(KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC /* Å */, 
                       KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,  KC_SCLN/* Ö */, KC_QUOT /* Ä */, 
-                      MOD_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, SE_LPRN,SE_SCLN,S(ALGR(SE_8)), SE_EQL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-                      S(SE_QUOT), KC_TAB, KC_LGUI, KC_BSPC, LT(_LOWER, KC_DEL), LT(_RAISE, KC_ENT), KC_SPC, KC_TAB, KC_BSPC, KC_RALT),
+                      KC_LEFT_CTRL, KC_Z, KC_X, KC_C, KC_V, KC_B, MACRO1,_______,_______,_______ , KC_N, KC_M, KC_COMM, KC_DOT, KC_BSLS, KC_MINS,
+                      KC_LEFT_ALT, KC_TAB, KC_LGUI, KC_BSPC, LT(_LOWER, KC_DEL), LT(_RAISE, KC_ENT), KC_SPC, S(SE_0), _______, _______),
     /*
-     * Lower Layer: Symbols
+     * Lower Layer: Symbol
      *
-     * ,-------------------------------------------.                              ,-------------------------------------------.
-     * |        |  !   |  "   |  Å   |  ^   |  *   |                              |      |      |      |      |      |    '   |
+     * ,-------------------------------------------.                              ,------------------------------------------.
+     * |        |  !   |  @   |  {   |  }   |  *   |                              |      |   7  |  8   |  9   |  *   |    '   |
      * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
-     * |        |  #   |  €   |  )   |  =   |  <   |                              |    ` |  +   |  -   |  (   |  %   |   ä   |
+     * |        |  #   |   "  |  (   |  )   |   =  |                              |   %  |   4  |   5  |  6   |   +  |    ´   |
      * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-     * |        |  %   |  &   |  å   |  ¨   |  >   |      |      |  |      |      |   /  |  ´   |  ,   |  .   |  -   | +      |
+     * |        |  %   |  &   |  [   |   ]  |   -  |      |      |  |      |      |   0  |  1   |  2   |  3   |  =   |        |
      * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
-     *                        |      |      |      |  ö   |      |  |   $  |  ö   |      |      |      |
+     *                        |   <  |  >   |      |      |      |  |   $  |  /   |   \  |      |      |
      *                        |      |      |      |      |      |  |      |      |      |      |      |
      *                        `----------------------------------'  `----------------------------------'
      */
-    [_LOWER] = LAYOUT(_______, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE, _______, _______, _______, _______, _______, KC_BSLS,
-                      _______, KC_HASH, KC_DLR, KC_LPRN, KC_RPRN, KC_GRV, KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOT, 
-                      _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______, _______, _______, _______, KC_AMPR, KC_EQL, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, 
-                      _______, _______, _______, KC_SCLN, KC_EQL, SE_DLR, KC_SCLN, _______, _______, _______),
+    
+    [_LOWER] = LAYOUT(_______, KC_EXLM, SE_AT, LALT(S(SE_8)),LALT(S(SE_9)), KC_PIPE, _______, KC_7, KC_8, KC_9,KC_PIPE, KC_BSLS,
+                      _______, KC_HASH, KC_AT,  KC_ASTR, KC_LPRN,S(SE_0), S(SE_5),KC_4,KC_5 ,KC_6,KC_MINS ,KC_EQL, 
+                      _______, KC_PERC, KC_CIRC, LALT(SE_8),LALT(SE_9), SE_MINS, _______,_______,_______ , _______ ,KC_0, KC_1,  KC_2,KC_3,S(SE_0) ,_______ , 
+                      KC_GRV, KC_TILD, _______, _______, _______, SE_DLR, S(SE_7),LALT(S(SE_7)), _______, _______),
     /*
      * Raise Layer: Number keys, media, navigation
      *
@@ -115,6 +121,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     //     ),
 };
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MACRO1:
+            if (record->event.pressed) {
+                rgblight_toggle();
+                rgblight_setrgb(RGB_RED);
+                // when keycode LYR_F13 is pressed
+                register_code(KC_LCTL);
+                register_code(KC_LEFT_ALT);
+                register_code(KC_LEFT_GUI);
+                register_code(KC_M); 
+            } else {
+                // when keycode LYR_F13 is released
+                unregister_code(KC_LCTL);
+                unregister_code(KC_LEFT_ALT);
+                unregister_code(KC_LEFT_GUI);
+                unregister_code(KC_M);  // release the F13 key
+}
+            break;
+    }
+    return true;
+}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
