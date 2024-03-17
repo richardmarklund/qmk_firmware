27aef7f455424cab5861894ab6bba8534ce9911e
Richard Marklund <richard.marklund@wirelesscar.com>
2024-03-17 12:51:35 +0100

    fix: fiddling

--- keyboards/cantor/keymaps/default/keymap.c
+++ keyboards/cantor/keymaps/default/keymap.c
@@ -20,23 +20,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │Ctl│ Z │ X │ C │ V │ B │       │ N │ M │ ,;│ .:│'* │ +?│
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
-      *               │GUI├───┐           ┌───┤Opt│
-      *               └───┤Tab├───┐   ┌───┤Bsp├───┘
+      *               │TAB├───┐           ┌───┤ = │
+      *               └───┤GUI├───┐   ┌───┤Opt├───┘
       *                   └───┤Ent│   │Spc├───┘
       *                       │Num├───┤Sgn│
       *                       └───┘   └───┘
       */                        
     [_QWERTY] = LAYOUT_split_3x6_3(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,            KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC ,
-        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,            KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,            KC_M,     KC_COMM, KC_DOT,  KC_BSLS, KC_MINS,
-                                            KC_TAB,  KC_LGUI, LT(_NUM,KC_ENT),        LT(_SIGN,KC_SPC),KC_BSPC, KC_LEFT_ALT 
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                      KC_Y,        KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC ,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                      KC_H,        KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                      KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_BSLS, KC_MINS,
+                                            KC_TAB,  LGUI_T(KC_BSPC), LT(_NUM,KC_ENT),        LT(_SIGN,KC_SPC), KC_LEFT_ALT, S(SE_0) 
+                                          
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │ * │ ! │ @ │ { │ } │ = │       │   │ < │ > │   │   │   │
+      * │   │ ! │ @ │ { │ } │ = │       │   │ < │ > │   │   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │   │ " │ ( │ ) │ - │       │ ; │ / │ \ │ | │ & │   │
+      * │   │ * │ " │ ( │ ) │ - │       │ ; │ / │ \ │ | │ & │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
       * │   │ % │ # │ [ │ ] │ _ │       │   │   │   │   │   │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
@@ -47,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [_SIGN] = LAYOUT_split_3x6_3(
-        SE_ASTR, KC_EXLM, SE_AT,   LALT(S(SE_8)), LALT(S(SE_9)), S(SE_0),                     KC_NO,      KC_GRV,  KC_TILD,       KC_NO,      KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_AT,   KC_ASTR,       KC_LPRN,       SE_MINS,                     S(KC_COMM), S(SE_7), LALT(S(SE_7)), LALT(SE_7), SE_AMPR, KC_NO,
-        KC_NO,   KC_PERC, KC_HASH, LALT(SE_8),    LALT(SE_9),    SE_UNDS,                     KC_NO,      KC_NO,   KC_NO,         KC_NO,      KC_NO,   KC_NO,
-                                                  KC_LGUI,       KC_BSPC, KC_ENT,     KC_NO,  KC_TAB, S(SE_0)
+        KC_NO, KC_EXLM, SE_AT,   LALT(S(SE_8)), LALT(S(SE_9)), S(SE_0),                     KC_NO,      KC_GRV,  KC_TILD,       KC_NO,      KC_NO,   KC_NO,
+        KC_NO, SE_ASTR, KC_AT,   KC_ASTR,       KC_LPRN,       SE_MINS,                     S(KC_COMM), S(SE_7), LALT(S(SE_7)), LALT(SE_7), SE_AMPR, KC_NO,
+        KC_NO, KC_PERC, KC_HASH, LALT(SE_8),    LALT(SE_9),    SE_UNDS,                     KC_NO,      KC_NO,   KC_NO,         KC_NO,      KC_NO,   KC_NO,
+                                                KC_LGUI,       KC_BSPC, KC_ENT,     KC_NO,  KC_TAB, S(SE_0)
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
